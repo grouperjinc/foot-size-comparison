b04f762e0b8e2e74ea595ecf5ddf9d926c7bea34
groupejrinc <grouperjinc@gmail.com>
2025-04-09 13:39:55 -0400

    Fix Mongoose model and collection naming

--- backend/models/Celebrity.js
+++ backend/models/Celebrity.js
@@ -2,17 +2,20 @@
 import mongoose from 'mongoose';
 
 const celebritySchema = new mongoose.Schema({
-  name: String,
-  shoeSize: Number,
-  image: String,
-  category: String, // Actor, Athlete, Music, etc.
-});
-
+    name: String,
+    shoeSize: Number,
+    image: String,
+    category: String,
+  }, { collection: 'celebrities' }); // ✅ match the real collection name
+  
 
 // what I am doing for serverless testing
-export default mongoose.models.Celebrity || mongoose.model('Celebrity', CelebritySchema);
+export default mongoose.models.celebrity || mongoose.model('celebrity', celebritySchema);
+  
 
 /* what I had before serverless
 const Celebrity = mongoose.model('Celebrity', celebritySchema);
 export default Celebrity;
 */
+
+
