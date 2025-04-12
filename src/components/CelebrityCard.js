df73ad4ca0c13298c06b02631d8942f81c8780e2
groupejrinc <grouperjinc@gmail.com>
2025-04-12 00:40:12 -0400

    CelebrityCard update for placeholders

--- src/components/CelebrityCard.js
+++ src/components/CelebrityCard.js
@@ -1,11 +1,17 @@
-// src/components/CelebrityCard.js
 import React from 'react';
 import './CelebrityCard.css';
 
 const CelebrityCard = ({ name, shoeSize, image, category }) => {
+  const placeholderImage = 'https://res.cloudinary.com/dlratz7ov/image/upload/v1744428483/placeholder_px9a1k.png'; // Placeholder image URL
+
   return (
     <div className="celebrity-card">
-      <img src={image} alt={name} className="celebrity-image" />
+      <img 
+        src={image || placeholderImage}  // Use celebrity image or the placeholder
+        alt={name}
+        className="celebrity-image"
+        onError={(e) => e.target.src = placeholderImage} // Fallback to placeholder if the image fails to load
+      />
       <div className="celebrity-name">{name}</div>
       <div className="celebrity-shoe-size">Shoe Size: {shoeSize}</div>
       <div className="celebrity-category">Category: {category}</div>
