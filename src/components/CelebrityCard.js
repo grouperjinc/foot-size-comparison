8c19821f5a248a5e41173a811958e3bf03bc9706
groupejrinc <grouperjinc@gmail.com>
2025-04-12 17:47:36 -0400

    remove image reference from frontend

--- src/components/CelebrityCard.js
+++ src/components/CelebrityCard.js
@@ -1,17 +1,9 @@
 import React from 'react';
 import './CelebrityCard.css';
 
-const CelebrityCard = ({ name, shoeSize, image, category }) => {
-  const placeholderImage = 'https://res.cloudinary.com/dlratz7ov/image/upload/v1744428483/placeholder_px9a1k.png'; // Placeholder image URL
-
+const CelebrityCard = ({ name, shoeSize, category }) => {
   return (
     <div className="celebrity-card">
-      <img 
-        src={image || placeholderImage}  // Use celebrity image or the placeholder
-        alt={name}
-        className="celebrity-image"
-        onError={(e) => e.target.src = placeholderImage} // Fallback to placeholder if the image fails to load
-      />
       <div className="celebrity-name">{name}</div>
       <div className="celebrity-shoe-size">Shoe Size: {shoeSize}</div>
       <div className="celebrity-category">Category: {category}</div>
