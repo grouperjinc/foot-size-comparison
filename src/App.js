bc1ba06f8430e9440de3db0e195da5365c2772c8
groupejrinc <grouperjinc@gmail.com>
2026-03-14 22:20:05 -0400

    Add homepage link to celebrity shoe size chart

--- src/App.js
+++ src/App.js
@@ -265,6 +265,7 @@ function App() {
             <a href="/about.html" style={footerLinkStyle}>About</a> |{" "}
             <a href="/terms.html" style={footerLinkStyle}>Terms of Use</a> |{" "}
             <a href="/privacypolicy.html" style={footerLinkStyle}>Privacy Policy</a>
+            <a href="/celebrity-shoe-size-chart/" style={footerLinkStyle}>Celebrity Shoe Size Chart</a>
                   </p>
           <p style={{ marginTop: '0.25rem', color: '#777' }}>© {new Date().getFullYear()} footsizecomparison.com</p>
         </footer>
